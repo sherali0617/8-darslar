626aec0385b509ee2e34be84efb27381f810849b
sherali0617 <begaliyevsherali625@gmail.com>
2026-05-06 18:01:14 +0500

    vazifa

---
+++ 1.py
@@ -0,0 +1,59 @@
+import os
+os.system("cls")
+
+# try:
+#     a=int(input("Son:"))
+#     b=int(input("Son:"))
+
+#     print(a/b)
+# except ValueError:
+#     print("Xato !! Son kiriting iltimos")
+# except ZeroDivisionError:
+#     print("Xatooo 0ga bolish mumkin emas")
+# else:
+#     print("Dastur bajarildi!!")
+# finally:
+#     print("try exept tugadi")
+
+
+# import math
+
+# print(math.pow(5, 3))
+
+# print(math.sqrt(25))
+
+# print(math.log2(16))
+# print(math.log(625, 5))
+
+# print(math.floor(2.95))
+# print(math.ceil(4.01))
+# print(round(2.95), round(4.51))
+
+
+# import random as rd
+
+# n = rd.randint(1,100)
+# print(n)
+
+# # m = rd.uniform(1, 10)
+# # print(m)
+
+# names = ['Samandar', 'Sherali', 'Doston', 'Begzod', 'Suhrob']
+# print(rd.choice(names))
+# print(rd.choices(names, k=2))
+# print(rd.sample(names, k=2))
+
+# rd.shuffle(names)
+# print(names)
+
+
+# os.mkdir("Salom")
+os.rmdir("Salom")
+
+os.remove("1.c")
+
+# qayta nomlash 1-fayl nomi keyn yangi nom
+os.rename("","") 
+
+
+
